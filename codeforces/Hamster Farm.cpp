28c4708af1ad423c8b9379599bf70f59fefce3d0
dipty13 <13dipty@gmail.com>
2020-01-07 22:43:51 +0600

    *Solved 1 problem from codeforces

--- codeforces/Hamster Farm.cpp
+++ codeforces/Hamster Farm.cpp
@@ -3,31 +3,41 @@
 #define mi map<int,int>
 #define mcr map<char,int>
 using namespace std;
-pair<ll,ll> solve(ll l1, ll r1, ll l2, ll r2){
-    if(l1 != l2){
+pair<ll,ll> solve(ll l1, ll r1, ll l2, ll r2)
+{
+    if(l1 != l2)
+    {
         return {l1,l2};
-    }else{
+    }
+    else
+    {
         return {l1,l2 + 1};
     }
 }
 int main()
 {
     ll n, m, i, j, k;
-    while(cin >> n >> k){
-      vector<ll> a(k + 5);
-      map<ll,ll> p;
-      vector<pair<ll,ll> > mp(k + 5);
-      for(i = 0; i < k; i++){
-        cin >> a[i];
-        p[a[i]] = 1;
-      }
-      sort(a.begin(), a.end());
-      for(i = 0; i < k; i++){
-        if(n % a[i] == 0)
-            cout << 1 << " " << 1 << endl;
-        else
+    while(cin >> n >> k)
+    {
+        vector<ll> a(k + 5);
+        map<ll,ll> p;
+         ll x = n, y = 0, tempH = INFINITY, h = 0, c = 0, tempCnt = 0;
+        for(i = 0; i < k; i++)
+        {
+            cin >> a[i];
+        }
+        for(i = 0; i < k; i++)
+        {
+            if( n % a[i] <= tempH){
+                tempH = n % a[i];
+                tempCnt = n / a[i];
+                c = i +1;
+            }
 
-      }
+        }
+        //if(l){
+        cout << c << " " << tempCnt<< endl;
+        //}
     }
 
     return 0;
