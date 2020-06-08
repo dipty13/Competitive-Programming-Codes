54abdaa0dd372e22dd0127388d629e3d9f44ff05
dipty13 <13dipty@gmail.com>
2020-06-08 21:49:00 +0600

    *Solved 1 problem

--- codeforces/Trouble sort.cpp
+++ codeforces/Trouble sort.cpp
@@ -13,40 +13,21 @@ int main()
         cin >> n;
         vector<ll> a(n + 5, INT_MAX);
         vector<ll> b(n + 5);
+        int flag = 1,c = 0, d = 0;
         for(int i = 0; i < n; i++){
             cin >> a[i];
+            if(i > 0 && a[i] < a[i - 1])
+                flag = 0;
         }
         for(int i = 0; i < n; i++){
             cin >> b[i];
+            if(b[i] == 1)
+                c = 1;
+            else if(b[i] == 0)
+                d = 1;
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(b[i] != b[j]){
-                    if(j < i && a[j] > a[i]){
-                        swap(a[j], a[i]);
-                        swap(b[j], b[i]);
-                    }else  if(j > i && a[i] > a[j]){
-                        swap(a[j], a[i]);
-                        swap(b[j], b[i]);
-                    }
-                }
-            }
-        }
-        vector<int> p(n + 5, INT_MAX);
-        for(int i = 0; i < n; i++){
-            //cout << a[i] << " ";
-            p[i] = a[i];
-        }
-        //cout << endl;
-        sort(p.begin(), p.end());
-        int flag = 1;
-        for(int i = 0; i < n; i++){
-           if(p[i] != a[i]){
-                flag = 0;
-                break;
-           }
-        }
-        if(flag)
+
+        if((c == 1 && d == 1) || flag == 1)
             cout << "Yes\n";
         else
             cout << "No\n";
