08c1e943b3e85383d5ff269ee0635b0afe0b515d
dipty13 <13dipty@gmail.com>
2020-06-11 22:48:45 +0600

    * Solved a problem

--- codeforces/b.cpp
+++ codeforces/b.cpp
@@ -3,84 +3,15 @@
 #define mi map<int,int>
 #define mcr map<char,int>
 using namespace std;
-
 int main()
 {
-    ll n, t, k, m, a, b, c, o, e;
+    ll n, t, k, m, a, b, c;
     cin >> t;
     while(t--)
     {
         cin >> n >> m;
-        vector<vector<ll> > v(n, vector<ll>(m, 0));
-        map<int, int> p;
-        map<int, int> q;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cin >> v[i][j];
-            }
-        }
-        int i = 0, j = 0, c = 0;
-        while( i < n && j < m){
-                int k = i, l = j, flag = 0;
-            while(k >= 0){
-                if(v[k][j] == 1){
-                    flag = 1;
-                    break;
-                }
-                k--;
-            }
-            if(flag){
-                j++;
-                continue;
-            }
-            k  = i;
-            while(k < n){
-                if(v[k][j] == 1){
-                    flag = 1;
-                    break;
-                }
-                k++;
-            }
-            if(flag){
-                j++;
-                continue;
-            }
-             while(l >= 0){
-                if(v[i][l] == 1){
-                    flag = 1;
-                    break;
-                }
-                l--;
-            }
-            if(flag){
-                i++;
-                continue;
-            }
-            l = j;
-            while(l < m){
-                if(v[i][l] == 1){
-                    flag = 1;
-                    break;
-                }
-                l++;
-            }
-            if(flag){
-                i++;
-                continue;
-            }
-            if(flag == 0){
-                c = !c;
-                //cout << i << " " << j << " " << v[i][j] << endl;
-                v[i][j] = 1;
-                i++;
-                j++;
-            }
-        }
-        if(c)
-            cout << "Ashish\n";
-        else
-            cout << "Vivek\n";
-
+        cout << min(min(n,m), (n + m)/ 3) << endl;
     }
+
         return 0;
     }
